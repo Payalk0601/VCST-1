66f3971cf2687bdc9510b8d624301a9e865c9b6a
Jayesh Lahare <jayehlahare1428@gmail.com>
2025-02-28 15:00:35 +0530

    jayesh

---
+++ Multiplication.py
@@ -0,0 +1,10 @@
+# Taking input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Multiplying the numbers
+product = num1 * num2
+
+# Displaying the result
+print("The product is:", product)
+
