ef6fed00314e11d5ec929c85a4e88e7beafaddc5
SaiUdawant <saiudawant2@gmail.com>
2025-02-28 14:45:05 +0530

    Update and rename Addition.py to Multiplication.py

--- Multiplication.py
+++ Multiplication.py
@@ -2,8 +2,9 @@
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
-# Adding the numbers
-sum_result = num1 + num2
+# Multiplying the numbers
+product = num1 * num2
 
 # Displaying the result
-print("The sum is:", sum_result)
+print("The product is:", product)
+
